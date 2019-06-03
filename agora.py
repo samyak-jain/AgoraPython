372b424cac9cfea0b852df1be7def080e7bdd7b3
samyak-jain <samtan106@gmail.com>
2019-06-04 03:01:48 +0530

    Inital Commit

---
+++ agora.py
@@ -0,0 +1,25 @@
+class AgoraRTC:
+    def __init__(self, app_id: str, channel_name: str):
+        self.app_id = app_id
+        self.channel_name = channel_name
+
+    def get_users(self):
+        pass
+
+
+class User:
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+    def close(self):
+        pass
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
